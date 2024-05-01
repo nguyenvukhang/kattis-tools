c20ab499885430619baee2b54f3b3573250fa286
nguyenvukhang <brew4k@gmail.com>
2024-05-01 21:55:02 +0800

    Summarize

--- __types__.py
+++ __types__.py
@@ -13,7 +13,7 @@ Verdict = Literal[
     "OLE",  # Output Limit Exceeded
     "AC",  # Accepted
 ]
-VERDICTS = get_args(Verdict)
+VERDICTS: list[Verdict] = get_args(Verdict)
 
 
 def v_rank(v: Verdict) -> int:
@@ -91,6 +91,15 @@ class Kattis:
     def list_student_usernames(self) -> list[str]:
         return [s["username"] for s in self.students]
 
+    def assert_valid_usernames(self, usernames: list[str]):
+        """
+        Checks that all usernames in `usernames` are found in the
+        database.
+        """
+        all_usernames = set(self.list_student_usernames())
+        for username in usernames:
+            assert username in all_usernames
+
     def list_assignments(self) -> list[str]:
         """
         Lists all the unique assignment names found in the database.
@@ -105,10 +114,67 @@ class Kattis:
     def subs(self) -> Iterator[tuple[str, Submission]]:
         """
         Create an iterator over the submissions of the assignment.
-        Returns a tuple of (<Assignment Name>, <Submission>) each iterate.
+        Returns a tuple of (<Assignment Name>, <Submission>) each
+        iterate.
         """
         for asm in self.assignments:
             for group in asm["groups"]:
                 for asm_name, result in group["results"].items():
                     for sub in result["submissions"]:
                         yield asm_name, sub
+
+    def get_results(
+        self, assignment_name: str, usernames: list[str]
+    ) -> dict[str, Verdict]:
+        """
+        Returns a list of key-value pairs with username as keys and
+        the verdict for `assignment_name` as values.
+        """
+
+        username_set = set(usernames)
+
+        # narrow down the search
+        subs = self.subs()
+        subs = filter(lambda v: v[0] == assignment_name, subs)
+        subs = map(lambda v: v[1], subs)
+        subs = filter(lambda v: v["submitting_user"] in username_set, subs)
+
+        verdicts: dict[str, Verdict] = {}
+
+        for sub in subs:
+            current_verdict = verdicts.get(sub["submitting_user"])
+            if current_verdict is None:
+                verdicts[sub["submitting_user"]] = sub["verdict"]
+            elif v_rank(sub["verdict"]) >= v_rank(current_verdict):
+                verdicts[sub["submitting_user"]] = sub["verdict"]
+
+        return verdicts
+
+    def get_summary(
+        self, assignment_name: str, usernames: list[str]
+    ) -> list[tuple[Verdict, list[str]]]:
+        """
+        Returns a list of key-value pairs with verdicts as keys and
+        the list of students who achieved that verdict as values.
+        """
+        return summarize(self.get_results(assignment_name, usernames))
+
+
+def summarize(results: dict[str, Verdict]) -> list[tuple[Verdict, list[str]]]:
+    summary = {v: [] for v in VERDICTS}
+    for username, v in results.items():
+        summary[v].append(username)
+    for v in VERDICTS:
+        if len(summary[v]) == 0:
+            del summary[v]
+    summary = list(summary.items())
+    summary.sort(key=lambda v: v_rank(v[0]))
+    return summary
+
+
+def lines(filepath):
+    try:
+        with open(filepath, "r") as f:
+            return f.read().splitlines()
+    except FileNotFoundError:
+        print(f"Warning: {filepath} not found")
--- main.py
+++ main.py
@@ -1,38 +1,10 @@
 from __types__ import *
 
-KATTIS_EXPORT_JSON_PATH = "export.json"
-
 kat = Kattis()
-kat.load(KATTIS_EXPORT_JSON_PATH)
-
-# dict[<username>, dict[<assignment>, <verdict>]]
-results: dict[str, dict[str, Verdict]] = {s["username"]: {} for s in kat.students}
+kat.load("export.json")
 
 # list of kattis usernames of students in class.
-my_class_usernames = [
-]
+my_class_usernames = lines("secrets.txt") or []
+kat.assert_valid_usernames(my_class_usernames)
 
-# for asm in assignments:
-#     for asm_name, sub in subs(asm):
-#         username = sub["submitting_user"]
-#         user_data = results[username]
-#         curr_verdict = user_data.get(asm_name, None)
-#         if curr_verdict is None:
-#             user_data[asm_name] = sub["verdict"]
-#             continue
-#         if v_rank(sub["verdict"]) >= v_rank(curr_verdict):
-#             user_data[asm_name] = sub["verdict"]
-
-
-def summarize(assignment: str, students: list[str]) -> dict[Verdict, list[str]]:
-    summary: dict[Verdict, list[str]] = {v: [] for v in VERDICTS}
-
-    for username in my_class_usernames:
-        user_data = results[username]
-        verdict = user_data.get("t9spelling", None)
-        if verdict is not None:
-            summary[verdict].append(username)
-    return summary
-
-
-print(kat.list_student_usernames())
+print(kat.get_summary("t9spelling", my_class_usernames))
